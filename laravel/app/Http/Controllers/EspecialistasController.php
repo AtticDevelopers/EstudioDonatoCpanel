319aeb7f684935cd14c25bf3f9809dcac0830ba3
mmassetti <matiasmassetti@gmail.com>
2019-12-03 13:52:34 -0300

    Cambio en los especialistas, algunas areas no tienen asique se deja vacio el especialista

--- laravel/app/Http/Controllers/EspecialistasController.php
+++ laravel/app/Http/Controllers/EspecialistasController.php
@@ -8,13 +8,19 @@ use App\Http\Controllers\Helpers\Abogado;
 
 class EspecialistasController extends Controller
 {
-    public function getEspecialistasView(){
+    public function getEspecialistasView()
+    {
         $especialistas = DB::select('SELECT * FROM especialistas');
         $areasEspecialistas = DB::select('SELECT * FROM areas NATURAL JOIN areas_especialistas NATURAL JOIN especialistas ORDER BY orden');
         $mediacionPrejudicial = DB::select('SELECT IDArea, descripcionLarga FROM areas WHERE nombreArea = \'Mediacion Prejudicial\' ');
-        
+        $aduanero = DB::select('SELECT IDArea, descripcionLarga FROM 
+            areas WHERE nombreArea = \'Aduanero\' ');
+        $societario = DB::select('SELECT IDArea, descripcionLarga FROM areas WHERE nombreArea = \'Societario\' ');
+        $concursal = DB::select('SELECT IDArea, descripcionLarga FROM areas WHERE nombreArea = \'Concursal\' ');
+
+
         $arreglo_areas = array();
-        foreach($areasEspecialistas as &$area){
+        foreach ($areasEspecialistas as &$area) {
 
             $abogado = new Abogado();
             $abogado->nombre = $area->nombre;
@@ -22,7 +28,7 @@ class EspecialistasController extends Controller
             $abogado->telefono = $area->telefono;
             $abogado->email = $area->email;
 
-            if (array_key_exists($area->nombreArea, $arreglo_areas)){
+            if (array_key_exists($area->nombreArea, $arreglo_areas)) {
                 $arreglo_viejo = $arreglo_areas[$area->nombreArea]['abogados'];
                 array_push($arreglo_viejo, $abogado);
                 $arreglo_abogados = $arreglo_viejo;
@@ -31,12 +37,21 @@ class EspecialistasController extends Controller
                 array_push($arreglo_abogados, $abogado);
                 $arreglo_general = array();
                 $arreglo_general['IDArea'] = $area->IDArea;
-                $arreglo_general['descripcionLarga'] = $area->descripcionLarga; 
+                $arreglo_general['descripcionLarga'] = $area->descripcionLarga;
             }
-            $arreglo_general['abogados'] = $arreglo_abogados; 
+            $arreglo_general['abogados'] = $arreglo_abogados;
             $arreglo_areas[$area->nombreArea] = $arreglo_general;
         }
         $arreglo_areas['Mediacion Prejudicial'] = array('IDArea' => $mediacionPrejudicial[0]->IDArea, 'descripcionLarga' => $mediacionPrejudicial[0]->descripcionLarga, 'abogados' => array());
+        $arreglo_areas['Aduanero'] = array('IDArea' =>
+        $aduanero[0]->IDArea, 'descripcionLarga' =>
+        $aduanero[0]->descripcionLarga, 'abogados' => array());
+        $arreglo_areas['Societario'] = array('IDArea' =>
+        $societario[0]->IDArea, 'descripcionLarga' =>
+        $societario[0]->descripcionLarga, 'abogados' => array());
+        $arreglo_areas['Concursal'] = array('IDArea' =>
+        $concursal[0]->IDArea, 'descripcionLarga' =>
+        $concursal[0]->descripcionLarga, 'abogados' => array());
 
         return view('especialistas', ['especialistas' => $especialistas, 'areas' => $arreglo_areas, 'page' => 'especialistas']);
     }
